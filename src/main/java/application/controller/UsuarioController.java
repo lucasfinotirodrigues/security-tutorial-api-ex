a7ba7bc5a4f7b8729ae5c8d16f0cd98d49ee9207
Lucas Finoti Rodrigues <lucasfinotirodrigues@hotmail.com>
2025-04-29 18:50:23 +0000

    fix: corrigindo erros

--- src/main/java/application/controller/UsuarioController.java
+++ src/main/java/application/controller/UsuarioController.java
@@ -1,10 +1,12 @@
 package application.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import application.record.GenericResponse;
 import application.record.UsuarioDTO;
@@ -18,6 +20,19 @@ public class UsuarioController {
     
     @PostMapping
     public GenericResponse insert (@RequestBody UsuarioDTO usuario) {
+        if(usuario.nodeDeUsuario().trim().length() == 0 ){
+            throw new ResponseStatusException(
+                HttpStatus.BAD_REQUEST, "Nome de usuário não definido"
+            );
+        }
+        if(usuario.senha().trim().length() == 0 ){
+            throw new ResponseStatusException(
+                HttpStatus.BAD_REQUEST, "Senha de usuário não definida"
+            );
+        }
+
+
+        
         return usuarioService.insert(usuario);
     }
 }
