4adbcf1d7b30232ce5b07b510711e47cc72a6866
Lucas Finoti Rodrigues <lucasfinotirodrigues@hotmail.com>
2025-04-29 17:25:42 +0000

    fix: corrigindo importação

--- src/main/java/application/controller/UsuarioController.java
+++ src/main/java/application/controller/UsuarioController.java
@@ -1,6 +1,7 @@
 package application.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,8 @@ import application.service.UsuarioService;
 public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
-
+    
+    @PostMapping
     public GenericResponse insert (@RequestBody UsuarioDTO usuario) {
         return usuarioService.insert(usuario);
     }
